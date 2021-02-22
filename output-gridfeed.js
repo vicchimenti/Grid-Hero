19eb4a2a0bd10110b13e5feb321c20e73c297393
vicchimenti <victor.chimenti@gmail.com>
2021-02-22 13:18:32 -0800

    wrap anchor tag

--- output-gridfeed.js
+++ output-gridfeed.js
@@ -52,6 +52,7 @@ try {
     var listItems = "";
     var listOfTags = "";
     var thumbNailString = "";
+    var anchorTagWrapper = '<div class="hidden">' + anchorTag + '</div>'
     var beginningHTML = '<div class="gridFeedItem card shadow col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-4" title="' + articleTitle + '" id="id<t4 type=\'meta\' meta=\'content_id\' data-position-default="ZoneA" data-position-selected="ZoneA" />">';
     var endingHTML = '</div>';
 
@@ -98,6 +99,21 @@ try {
     }
 
 
+    /***
+     *  Hide the anchor tag
+     * 
+     * */
+    if (frontPageImage == "") {
+        thumbNailString = '<span class="newsroomImageWrapper hidden">No Image Provided</span>';
+
+    } else {
+        thumbNailString = '<span class="newsroomImageWrapper"><img src="' + frontPageImage + '" class="articleImage card-img-top" alt="' + frontPageImageCaption + '" /></span>';
+    }
+
+
+
+
+
 
 
     /***
@@ -105,7 +121,8 @@ try {
      * 
      * */
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, beginningHTML));
-    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, anchorTag));
+    // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, anchorTag));
+    document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, anchorTagWrapper));
     document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, thumbNailString));
     // document.write(com.terminalfour.publish.utils.BrokerUtils.processT4Tags(dbStatement, publishCache, section, content, language, isPreview, photoCredit));
     document.write('<div class="newsroomArticleBlurb card-body">');
